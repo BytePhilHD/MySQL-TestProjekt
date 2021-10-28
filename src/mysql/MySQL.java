e7997707809585c8ae69796d9ce8613fcfb99e6d
BytePhil <phitho2018@gmail.com>
2021-10-28 20:33:13 +0200

    Testing sending and receiving byte arrays

--- src/mysql/MySQL.java
+++ src/mysql/MySQL.java
@@ -1,9 +1,13 @@
 package mysql;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.*;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.Scanner;
 
 public class MySQL {
@@ -32,6 +36,7 @@ public class MySQL {
         System.out.println("Result: " + rst.getInt("Tee"));
         System.out.println("");
         new MySQL().addFile("test.txt", stmt);
+        new MySQL().downloadFile(stmt);
         //addFile();
         /*
         System.out.println("Type something to put in the Hallo table.");
@@ -47,16 +52,41 @@ public class MySQL {
         try {
             File file = new File(getClass().getClassLoader().getResource(name).getFile());
             array = Files.readAllBytes(file.toPath());
+            System.out.println("Path " + file.toPath().toString());
             //System.out.println("Bytes: " + Arrays.toString(array));
             //stmt.executeUpdate("INSERT INTO Files (Filename, File) VALUES ('TestFile', '" + Arrays.toString(array) + "')");
-        } catch (Exception e1) {}
+        } catch (Exception e1) {
+        }
     }
 
     public void downloadFile(Statement stmt) throws SQLException {
         ResultSet rst = stmt.executeQuery("SELECT * FROM `Files`");
         rst.next();
         byte[] array = rst.getString("File").getBytes();
-       // Files.write(getClass().getClassLoader().getResource("test1.txt"), array);
+        System.out.println("Array: " + Arrays.toString(array));
+        String FILEPATH = "Files/test1.txt";
+        File file = new File(FILEPATH);
+
+        String s = Base64.getEncoder().encodeToString(array);
+        try {
+            byte[] decode = Base64.getDecoder().decode(s);
+            Files.write(Paths.get("Files/test1.txt"), decode);
+        } catch (Exception e1) { }
+
+        /*
+        try {
+            String str = new String(array, 0, 3, StandardCharsets.UTF_8);
+
+            OutputStream os = new FileOutputStream(file);
+
+            os.write(array);
+            System.out.println("Successfully byte inserted");
+
+            os.close();
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
+        */
     }
 
     public static void startMySQL() {
